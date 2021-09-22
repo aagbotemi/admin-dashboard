845263f45df850d3fa3c638b454968b26d43b8d4
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-09-22 22:49:05 +0100

    avatar dropdown implemented

---
+++ src/components/AvatarDropdown.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { AiOutlineSetting } from 'react-icons/ai'
+import { FiLogOut } from 'react-icons/fi'
+import { CgProfile } from 'react-icons/cg'
+
+const AvatarDropdown = () => {
+    return (
+        <ul className="bg-purple-50 absolute top-12 -right-2 rounded-md shadow-lg max-w-xs w-36">
+            <li className="px-3 hover:bg-blue-50">
+                <div className="flex items-center border-b py-2 hover:text-blue-400">
+                    <span className="">
+                        <CgProfile  size="22px" />
+                    </span>
+                    <span className="ml-2 text-md font-medium">
+                        Profile
+                    </span>
+                </div>
+            </li>
+            <li className="px-3 hover:bg-blue-50">
+                <div className="flex items-center border-b py-2 hover:text-blue-400">
+                    <span className="">
+                        <AiOutlineSetting  size="22px" />
+                    </span>
+                    <span className="ml-2 text-md font-medium">
+                        Setting
+                    </span>
+                </div>
+            </li>
+            <li className="px-3 hover:bg-red-50">
+                <div className="flex items-center border-b py-2">
+                    <span className="">
+                        <FiLogOut  size="22px" color="red" />
+                    </span>
+                    <span className="ml-2 text-md font-medium text-red-500 ">
+                        LogOut
+                    </span>
+                </div>
+            </li>
+        </ul>
+    )
+}
+
+export default AvatarDropdown
--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -5,32 +5,36 @@ import { IoIosNotificationsOutline } from 'react-icons/io';
 import { BsCheckCircle } from 'react-icons/bs';
 import { BiErrorCircle } from 'react-icons/bi';
 import Notification from './Notification';
-
-
-
+import AvatarDropdown from './AvatarDropdown';
 
 const Topbar = () => {
     const ref = useRef();
     const [showNotification, setShowNotification] = useState(false);
+    const [showAvatarDropdown, setShowAvatarDropdown] = useState(false);
 
-    const onClick = () => setShowNotification(true);
+    const handleNotification = () => setShowNotification(true);
+    const handleAvatarDropdown = () => setShowAvatarDropdown(true);
+    
 
     useEffect(() => {
         const checkIfClickedOutside = e => {
         // If the menu is open and the clicked target is not within the menu,
         // then close the menu
-            if (showNotification && ref.current && !ref.current.contains(e.target)) {
+            if (showAvatarDropdown && ref.current && !ref.current.contains(e.target) || showNotification && ref.current && !ref.current.contains(e.target)) {
                 setShowNotification(false)
+                setShowAvatarDropdown(false)
+            } else {
+                setShowNotification(false)
+                setShowAvatarDropdown(false)
             }
         }
-
         document.addEventListener("mousedown", checkIfClickedOutside)
 
         return () => {
         // Cleanup the event listener
         document.removeEventListener("mousedown", checkIfClickedOutside)
         }
-    }, [showNotification])
+    }, [showNotification, showAvatarDropdown])
 
     return (
         <div className="topbar h-14 bg-blue-300 flex items-center justify-between sticky top-0 md:px-12 sm:px-8 px-4 py-8" ref={ref}>
@@ -38,9 +42,9 @@ const Topbar = () => {
                 <img src={Logo} alt="" className="company-logo w-12" />
             </div>
             <div className="rightTopbar flex items-center">
-                <div className="topbarIconContainer flex items-center relative cursor-pointer bg-purple-100 rounded-full p-1">
-                    <span className="absolute -top-2 right-0 bg-red-600 text-white text-center text-xs rounded-full w-4 h-4">3</span>
-                    <IoIosNotificationsOutline size="21px" onClick={onClick} />
+                <div className="topbarIconContainer flex items-center relative cursor-pointer hover:bg-purple-200 bg-purple-100 rounded-full p-2">
+                    <span className="absolute -top-1 right-0 bg-red-600 text-white text-center text-xs rounded-full w-4 h-4">3</span>
+                    <IoIosNotificationsOutline size="21px" onClick={handleNotification} />
                     {showNotification 
                     && ( <Notification />)}
                 </div>
@@ -51,13 +55,12 @@ const Topbar = () => {
                     <span>3</span>
                 </div>
 
-                <div className="userImg border-l-2">
-                    <img src={Avatar} alt="" className="w-9 h-9 ml-3 rounded-full cursor-pointer" />
+                <div className="topbarIconContainer flex items-center relative cursor-pointer border-l-2">
+                    <img src={Avatar} alt="" className="w-9 h-9 ml-3 rounded-full cursor-pointer" onClick={handleAvatarDropdown} />
+                    {showAvatarDropdown 
+                    && ( <AvatarDropdown />)}
                 </div>
-
-
             </div>
-
         </div>
     )
 }
