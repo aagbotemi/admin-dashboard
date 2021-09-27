df2feee9fb1740bac687e540bd14f0e6d981b586
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-09-27 16:24:14 +0100

    chart added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import Chart from '../components/Chart'
 import FeaturedInfo from '../components/FeaturedInfo'
-
+import { userData } from './../dummyData'
 
 const Home = () => {
     return (
@@ -8,8 +9,11 @@ const Home = () => {
             <h1 className="mb-4 text-3xl font-semibold border-b-2 border-gray-300">Dashboard</h1>
             <FeaturedInfo />
 
-            <div>
-                
+            <div className="grid md:grid-cols-3 grid-cols-1 gap-4 mt-5">
+                <div className="col-span-2">
+                    <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
+                </div>
+                <div className="">1</div>
             </div>
         </div>
     )
