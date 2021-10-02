5ac25a173d567b473b4aa3bf9f8fdc9c61631019
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-10-02 19:41:19 +0100

    router link added to button

--- src/pages/UserDetail.js
+++ src/pages/UserDetail.js
@@ -5,6 +5,7 @@ import { FaPhoneAlt } from 'react-icons/fa'
 import { FiMail } from 'react-icons/fi'
 import { GrLocation } from 'react-icons/gr'
 import { userDetailsData } from '../dummyData'
+import { Link } from 'react-router-dom'
 
 
 
@@ -15,10 +16,12 @@ const UserDetail = () => {
         <div className="user-detail bg-gray-100 pt-3 sm:px-5 px-3">
             <div className="mb-4 pb-2 border-b-2 border-gray-300 flex items-center justify-between">
                 <h2 className="text-2xl font-semibold">User Details</h2>
-                <button className="bg-blue-600 px-2 py-1 text-white rounded-md text-sm capitalize sm:block hidden">Add new user</button>
-                <button className="bg-blue-600 px-4 py-1 text-white rounded-md capitalize sm:hidden"> 
-                    <AiOutlinePlus size="20px" />
-                </button>
+                <Link to="/new-user">
+                    <button className="bg-blue-600 px-2 py-1 text-white rounded-md text-sm capitalize sm:block hidden">Add new user</button>
+                    <button className="bg-blue-600 px-4 py-1 text-white rounded-md capitalize sm:hidden"> 
+                        <AiOutlinePlus size="20px" />
+                    </button>
+                </Link>
             </div>
 
             <div className="grid md:grid-cols-2 grid-cols-1 gap-3 mt-5 pb-8">
