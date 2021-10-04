d8b1c678a241888225df97d63cce1306886b6c04
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-10-04 18:30:19 +0100

    SEO file updated

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -6,7 +6,7 @@ import Favicon from '../assets/images/logo.svg';
 const SEO = ({title}) => {
     return (
         <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | Admin Dashboard`}>
-            <meta name="description" content="aagbotemi is an admin dashboard where you can updates about the daily activities in an organization" />
+            <meta name="description" content="aagbotemi is an admin dashboard where you can see updates about the daily activities in an organization" />
             <meta name="keywords" content="admin, dashboard, users, products, transactions &amp; details" />
             <meta name="image" content={Logo} />
             <link rel="shortcut icon" href={Favicon}/>
